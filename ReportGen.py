6699e9abcba3185ed800af175faf28eb656e2187
DigvijayDutt <duttdigvijay@gmail.com>
2026-03-06 11:51:28 +0530

    port binding

--- ReportGen.py
+++ ReportGen.py
@@ -651,5 +651,5 @@ class MultiDocApp:
 # Run app
 MultiDocApp()
 # port = int(os.environ.get("PORT", 8080))
-ui.run(host="127.0.0.1", port=8080, reload=False, workers=1)
+ui.run(host="0.0.0.0", port=8080, reload=False, workers=1)
 
